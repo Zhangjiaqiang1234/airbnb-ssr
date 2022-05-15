305f136d53fdb8a7502e45e4ccc0bbc930938236
Zhangjiaqiang1234 <810751959@qq.com>
2022-05-15 23:24:00 +0800

    集成 element-plus--按需引入组件

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import router from './router'
+import router from "./router";
 
-import ElementPlus from 'element-plus'
+import ElementPlus, { ElMessage } from "element-plus";
 /* 引入 element-ui 基础样式文件 */
-import 'element-plus/dist/index.css' 
+import "element-plus/dist/index.css";
 
 /* 创建应用实例 */
-const app = createApp(App)
+const app = createApp(App);
 
 /* 将路由挂载到根实例上 */
-app.use(router)
+app.use(router);
+
+/* 全局引入 element-ui 组件 */
+app.config.globalProperties.$message = ElMessage;
 
 /* 将 element-ui 挂载到根实例 */
-app.use(ElementPlus)
+app.use(ElementPlus);
 
-app.mount('#app')
+app.mount("#app");
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { useRouter, useRoute } from "vue-router";
+import { h, getCurrentInstance } from "vue";
 const router = useRouter();
-const route = useRoute()
-
+const route = useRoute();
 
 console.log(route.params);
 
-
+/* 因为 vue2 中是 this.$message 使用的，但是 vue3 中没有 this，所以需要创建一个上下文来处理 */
+const { proxy }: any = getCurrentInstance();
+proxy.$message({
+  message: h("p", null, [
+    h("span", null, "Message can be"),
+    h("i", { style: "color:teal" }, "Vnode"),
+  ]),
+});
 </script>
 
 <template>
